1c35d4d6a8cea1f67f58a3db193c1f2b1bc3908f
honoremihigo <mihigohonore@gmail.com>
2025-08-28 20:54:52 +0200

    finished department

---
+++ backend/prisma/migrations/20250828093730_department_model/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `department` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `description` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `department_id_key`(`id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
